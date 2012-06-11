1dfcc2545167549b023cfa065d571b4c83357b84
Emily Twist <emily.twist@btinternet.com>
2012-06-11 16:22:09 +0100

    add postgre gem and admin functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   
   validates :name, presence: true, uniqueness: true
   has_secure_password
+  
+  after_destroy :ensure_an_admin_remains
+  
+  private
+    
+    def ensure_an_admin_remails
+      if User.count.zero?
+        raise "Can't delete last user"
+      end
+    end
 end
