61faccaefc225787d5708a4927606a0eac3c5dfc
Emily Twist <emily.twist@btinternet.com>
2012-06-11 08:28:36 +0100

    Adding a cart

---
+++ app/models/line_item.rb
@@ -0,0 +1,14 @@
+class LineItem < ActiveRecord::Base
+  attr_accessible :cart_id, :product_id, :quantity
+  
+  belongs_to :product
+  belongs_to :cart
+  
+  def total_price
+    product.price * quantity
+  end
+  
+  def price
+    price = product.price
+  end
+end
